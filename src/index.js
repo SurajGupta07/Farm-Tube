a86dffc1d14278b1393a55e513657dbeedb06f01
SurajGupta07 <bgs071998@gmail.com>
2021-06-16 18:51:29 +0530

    Header component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createMuiTheme, ThemeProvider} from "@material-ui/core";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#333335" 
+        },
+        secondary: {
+          main: "#A78BFA"
+        }
+    }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+    <Router>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </Router>
+</React.StrictMode>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function to
+// log results (for example: reportWebVitals(console.log)) or send to an
+// analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
