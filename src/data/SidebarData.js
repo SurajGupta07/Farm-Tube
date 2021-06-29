860133ea744578541941d799a96633701358ae90
SurajGupta07 <bgs071998@gmail.com>
2021-06-29 18:54:25 +0530

    Refactor: Sidebardata

--- src/data/SidebarData.js
+++ src/data/SidebarData.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
+import { MdLibraryBooks } from "react-icons/md";
 
 export const SidebarData = [
   {
@@ -11,15 +11,15 @@ export const SidebarData = [
     cName: 'nav-text'
   },
   {
-    title: 'History',
-    path: '/history',
-    icon: <FaIcons.FaCartPlus />,
-    cName: 'nav-text'
-  },
-  {
     title: 'Liked Videos',
     path: '/liked',
     icon: <IoIcons.IoMdPeople />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'History',
+    path: '/history',
+    icon: <MdLibraryBooks />,
     cName: 'nav-text'
   }
 ];
